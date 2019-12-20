d7c7143927ad60f0832f715ceea51b5b6b567449
sirAlexander <alexander.shikanga.tindi@gmail.com>
2019-12-20 20:27:59 +0000

    manipulating state

--- react-the-complete-guide/shikanga-react-complete-guide/src/App.js
+++ react-the-complete-guide/shikanga-react-complete-guide/src/App.js
@@ -6,13 +6,21 @@ class App extends Component {
   state = {
     persons: [
       { name: 'Max', age: 28 },
-      { name: 'Manu, age 29' },
+      { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 26 }
-    ]
+    ],
+    otherState: 'some other value'
   }
 
   switchNameHandler = () => {
-    console.log('Was clicked!');
+    // console.log('Was clicked!');
+    this.setState({
+      persons: [
+        { name: 'Maximilian', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    })
   }
 
   render() {
