e8fddba1dc30f41bfdcfeb103202c82b9bc13bd5
sirAlexander <alexander.shikanga.tindi@gmail.com>
2019-12-31 20:08:50 +0000

    css modules and media queries

--- react-the-complete-guide/shikanga-react-complete-guide/src/App.js
+++ react-the-complete-guide/shikanga-react-complete-guide/src/App.js
@@ -5,20 +5,6 @@ import classes from './App.css';
 import Person from './Person/Person';
 
 
-// const StyledButton = styled.button`
-//   background-color: ${props => props.alt ? 'red' : 'green'};
-//   color: white;
-//   font: inherit;
-//   border: 1px solid blue;
-//   padding: 8px;
-//   cursor: pointer;
-
-//   &:hover {
-//     background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
-//     color: black;
-//   }
-// `;
-
 class App extends Component {
   state = {
     persons: [
