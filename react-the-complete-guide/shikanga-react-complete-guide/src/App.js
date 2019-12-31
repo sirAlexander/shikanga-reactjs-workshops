563ae69f2222bab51b70ed07285c41425c6091cb
sirAlexander <alexander.shikanga.tindi@gmail.com>
2019-12-31 11:45:27 +0000

    removed references to Radium

--- react-the-complete-guide/shikanga-react-complete-guide/src/App.js
+++ react-the-complete-guide/shikanga-react-complete-guide/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium, { StyleRoot } from 'radium';
+
 import Person from './Person/Person';
 
 class App extends Component {
@@ -89,7 +89,6 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
         <div className="App">
           <h1>Hi, I'm a React App</h1>
           <p className={classes.join(' ')}>This is really working!</p>
@@ -98,9 +97,8 @@ class App extends Component {
             onClick={this.togglePersonsHandler}>Toggle Persons</button>
           {persons}
         </div>
-      </StyleRoot>
     );
   }
 }
 
-export default Radium(App);
+export default App;
