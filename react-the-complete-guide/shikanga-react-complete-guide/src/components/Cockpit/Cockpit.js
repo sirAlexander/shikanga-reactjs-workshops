94f3b97ad6cfc414118c05474352ca437569a340
sirAlexander <alexander.shikanga.tindi@gmail.com>
2020-01-23 20:41:13 +0000

    an example of cleanup work while using useEffect()

--- react-the-complete-guide/shikanga-react-complete-guide/src/components/Cockpit/Cockpit.js
+++ react-the-complete-guide/shikanga-react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -5,10 +5,11 @@ const cockpit = (props) => {
     useEffect(() => {
         console.log('[Cockpit.js] useEffect');
         // Http request...
-        setTimeout(() => {
+        const timer = setTimeout(() => {
             alert('Saved data to cloud!');
         }, 1000);
         return () => {
+            clearTimeout(timer);
             console.log('[Cockpit.js] cleanup work in useEffect');
         }
     }, []);
