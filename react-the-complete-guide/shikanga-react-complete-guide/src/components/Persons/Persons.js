b7fe98bac7b5f25d1c23a3c1055917df128e248f
Alexander TIndi <>
2020-01-23 17:17:51 +0000

    Component Update Lifecycle (for props Changes)

--- react-the-complete-guide/shikanga-react-complete-guide/src/components/Persons/Persons.js
+++ react-the-complete-guide/shikanga-react-complete-guide/src/components/Persons/Persons.js
@@ -3,6 +3,34 @@ import React, { Component } from 'react';
 import Person from './Person/Person';
 
 class Persons extends Component {
+    // static getDerivedStateFromProps(props, state) {
+    //     console.log('[Persons.js] getDerivedStateFromProps');
+    //     return state;
+    // }
+
+    // componentWillReceiveProps(props){
+    //     console.log('[Persons.js] componentWillReceiveProps', props);
+    // }
+
+    // componentWillUpdate(){
+
+    // }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[Persons.js] shouldCompopnentUpdate');
+        return true;
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('[Persons.js] getSnapshotBeforeUpdate');
+        return { message: 'Snapshot!'};
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('[Persons.js] componentDidUpdate');
+        console.log(snapshot);
+    }
+
     render() {
         console.log('[Persons.js] rendering...');
         return (
