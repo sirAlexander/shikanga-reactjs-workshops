8cc33655dbe6ea7f2273ce892af85edf251dc296
sirAlexander <alexander.shikanga.tindi@gmail.com>
2020-01-23 21:06:18 +0000

    optimizing functional components with

--- react-the-complete-guide/shikanga-react-complete-guide/src/components/Cockpit/Cockpit.js
+++ react-the-complete-guide/shikanga-react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -27,10 +27,10 @@ const cockpit = (props) => {
     if (props.showPersons) {
         btnClass = classes.Red;
     }
-    if (props.persons.length <= 2) {
+    if (props.personsLength <= 2) {
         assignedClasses.push(classes.red); // assignedClasses = ['red']
     }
-    if (props.persons.length <= 1) {
+    if (props.personsLength <= 1) {
         assignedClasses.push(classes.bold); // assignedClasses = ['red', 'bold']
     }
     return (
@@ -44,4 +44,4 @@ const cockpit = (props) => {
     );
 };
 
-export default cockpit;
+export default React.memo(cockpit);
