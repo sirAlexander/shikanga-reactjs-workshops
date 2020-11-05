be0366c5da6ebed94218e9a3a8f2464abd5cdb03
sirAlexander <alexander.shikanga.tindi@gmail.com>
2020-11-05 21:49:44 +0000

    Helpers: routes (React router auth)

---
+++ netflix-clone/netflix/src/helpers/routes.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+
+export function IsUserRedirect({ user, loggedInPath, children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        if (!user) {
+          return children;
+        }
+
+        if (user) {
+          return (
+            <Redirect
+              to={{
+                pathname: loggedInPath,
+              }}
+            />
+          );
+        }
+        return null;
+      }}
+    />
+  );
+}
