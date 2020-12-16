5f950df8a3d6a77e9efd77bb39ee0e437dfb5815
sirAlexander <alexander.shikanga.tindi@gmail.com>
2020-12-16 19:30:14 +0000

    Spinner review

--- netflix-clone/netflix/src/containers/browse.js
+++ netflix-clone/netflix/src/containers/browse.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext, useEffect } from "react";
 import { SelectProfileContainer } from "./profiles";
 import { FirebaseContext } from "../context/firebase";
+import { Header, Loading } from "../components";
 
 export function BrowseContainer({ slides }) {
   const [profile, setProfile] = useState({});
@@ -15,5 +16,15 @@ export function BrowseContainer({ slides }) {
     }, 3000);
   }, [profile.displayName]);
 
-  return <SelectProfileContainer user={user} setProfile={setProfile} />;
+  return profile.displayName ? (
+    <>
+      {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+
+      <Header src="joker1">
+        <p>Hello</p>
+      </Header>
+    </>
+  ) : (
+    <SelectProfileContainer user={user} setProfile={setProfile} />
+  );
 }
